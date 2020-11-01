eda5dc7956ddcbf44cc127cb99069e07a484fc18
Estevan Jantsk <estevan.jantsk@gmail.com>
2020-11-01 15:58:14 +0200

    integrate react-day-picker with moment

--- components/DateRangePicker.js
+++ components/DateRangePicker.js
@@ -1,33 +1,68 @@
+import { useState } from "react";
 import DayPickerInput from "react-day-picker/DayPickerInput";
+import LocaleUtils from "react-day-picker/moment";
 import "react-day-picker/lib/style.css";
 
-const DayRangePicker = () => (
-  <div className="date-range-picker-container">
-    <div>
-      <label>From:</label>
-      <DayPickerInput />
-    </div>
-    <div>
-      <label>To:</label>
-      <DayPickerInput />
-    </div>
+const format = "DD MMM yyyy";
 
-    <style jsx>{`
-      .date-range-picker-container div {
-        display: grid;
-        border: 1px solid #ddd;
-        grid-template-columns: 30% 70%;
-        padding: 10px;
-      }
-    `}</style>
-    <style jsx global>{`
-      .DayPickerInput input {
-        width: 120px;
-        padding: 10px;
-        font-size: 16px;
-      }
-    `}</style>
-  </div>
-)
+const DayRangePicker = () => {
+  const [startDate, setStartDate] = useState(new Date());
+  const [endDate, setEndDate] = useState(new Date());
+  
+  return (
+    <div className="date-range-picker-container">
+      <div>
+        <label>From:</label>
+        <DayPickerInput 
+          formatDate={LocaleUtils.formatDate}
+          parseDate={LocaleUtils.parseDate}
+          placeholder={`${LocaleUtils.formatDate(new Date(), format)}`}
+          format={format}
+          dayPickerProps={{
+            disabledDays: {
+              before: new Date()
+            }
+          }}
+          onDayChange={(day) => {
+            setStartDate(day);
+          }}
+        />
+      </div>
+      <div>
+        <label>To:</label>
+        <DayPickerInput 
+          formatDate={LocaleUtils.formatDate}
+          parseDate={LocaleUtils.parseDate}
+          placeholder={`${LocaleUtils.formatDate(new Date(), format)}`}
+          format={format}
+          dayPickerProps={{
+            disabledDays: {
+              before: new Date()
+            }
+          }}
+          onDayChange={(day) => {
+            setEndDate(day);
+          }}
+        />
+      </div>
+  
+      <style jsx>{`
+        .date-range-picker-container div {
+          display: grid;
+          border: 1px solid #ddd;
+          grid-template-columns: 30% 70%;
+          padding: 10px;
+        }
+      `}</style>
+      <style jsx global>{`
+        .DayPickerInput input {
+          width: 120px;
+          padding: 10px;
+          font-size: 16px;
+        }
+      `}</style>
+    </div>
+  )
+}
 
 export default DayRangePicker;
