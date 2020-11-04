8b670d763ca1a5363813b31fe0a343b93c47989d
Estevan Jantsk <estevan.jantsk@gmail.com>
2020-11-04 23:19:36 +0200

    add sessionStore

--- server.js
+++ server.js
@@ -1,5 +1,12 @@
 const express = require('express')
 const next = require('next')
+const session = require('express-session')
+const SequelizeStore = require('connect-session-sequelize')(session.Store)
+const User = require('./model').User
+const sequelize = require('./model').sequelize
+
+const sessionStore = new SequelizeStore({ db: sequelize, tableName: 'sessions' })
+// sessionStore.sync()
 
 const port = parseInt(process.env.PORT, 10) || 3000
 const dev = process.env.NODE_ENV !== 'production'
@@ -8,6 +15,18 @@ const handle = nextApp.getRequestHandler()
 
 nextApp.prepare().then(() => {
   const server = express()
+
+  server.use(session({
+    secret: '2d0fdcee-c721-42ea-a796-7cfe85221b9a',
+    resave: false,
+    saveUninitialized: true,
+    name: 'nextbnb',
+    cookie: {
+      secure: false,
+      maxAge: 30 * 24 * 60 * 60 * 1000 // 30 days
+    },
+    store: sessionStore
+  }))
 
   server.all('*', (req, res) => {
     return handle(req, res)
