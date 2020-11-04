99c618d29dab494555e56b50b6ea23375b246feb
Estevan Jantsk <estevan.jantsk@gmail.com>
2020-11-04 23:08:55 +0200

    trying to improve hmr

--- components/Header.js
+++ components/Header.js
@@ -11,7 +11,7 @@ const Header = () => {
 
   return (
     <div className="nav-container">
-    <Link href="/">
+    <Link href="/"> 
       <a>
         <img src="/img/logo.png" alt="Logo"/>
       </a>
